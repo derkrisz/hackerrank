445b69ec3158b29e75b269391ffc09e4caba3967
Krisztian Der <krisztian.der@gmail.com>
2021-08-29 08:56:09 +0100

    lisa's workbook

---
+++ Python/Lisa's Workbook /lisas_workbook.py
@@ -0,0 +1,48 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'workbook' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER k
+#  3. INTEGER_ARRAY arr
+#
+
+def workbook(n, k, arr):
+    page = special = 0
+    
+    for i in arr:
+        page += 1
+        for num in range(1, i + 1 ):
+            if num == page:
+                special += 1
+            if num % k == 0 and num < i:
+                page += 1
+                
+    return special
+            
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    k = int(first_multiple_input[1])
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = workbook(n, k, arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
